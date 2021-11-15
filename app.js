8c69c34bb589d67c4401c35dbae35b517f0985d3
devyosse <narutoshippuden884@gmail.com>
2021-11-15 10:32:27 +0100

    exo 56

---
+++ app.js
@@ -0,0 +1,25 @@
+let maVariable = prompt('entrez le nom dun accessoire')
+
+switch (maVariable){
+    case 'souris':
+        alert("J'utilise une souris")
+        break;
+    case 'clavier':
+        alert("j'utilise un clavier")
+        break;
+    case 'ordinateur':
+        alert("j'utilise un clavier")
+        break;
+    case 'tapis':
+        alert("j'utilise un tapis")
+        break;
+    case 'imprimante':
+        alert("j'utilise une imprimante")
+        break;
+    case 'ondulateur':
+        alert("j'utilise un onculateur")
+        break;
+    default:
+        alert("Utilisation inconnu")
+}
+
